c66a6bb15d70f1362495e4b98c56a9df40278a41
Mike <15527358+devdot4@users.noreply.github.com>
2021-07-29 04:03:30 -0500

    update aws_security_group.tf

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -1,14 +1,14 @@
-resource "aws_security_group" "sec-group-team4" {
+resource "aws_security_group" "aws_sg" {
   name        = var.aws_security_group_name
   description = var.aws_security_group_description
-  vpc_id      = module.vpc1.vpc
+  vpc_id      = module.vpc.vpc
   dynamic "ingress" {
     for_each = var.aws_security_group_service_ports
     content {
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
-      cidr_blocks = var.aws_security_group_cidr_block
+      cidr_blocks = [var.aws_security_group_cidr_block]
     }
   }
   egress {
