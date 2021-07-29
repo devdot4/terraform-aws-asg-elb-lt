14b482459d7004e5f7d8082edc617645cf32a25f
Mike <15527358+devdot4@users.noreply.github.com>
2021-07-28 21:57:45 -0500

    update module.tf

--- module.tf
+++ module.tf
@@ -12,12 +12,12 @@ module "vpc1" {
   tags          = var.aws_tags
 }
 module "db" {
+  version                 = "1.0.7"
   source                  = "maxat2416/RDS-project/aws"
-  version                 = "1.0.6"
   name                    = "rds_db_team3_are_the_best"
   engine                  = "aurora"
   engine_version          = "5.6.10a"
-  instance_class          = "db.r4.large"
+  instance_class          = "db.t3.small"
   vpc_id                  = module.vpc1.vpc
   subnet_ids              = module.vpc1.private_subnets
   allowed_security_groups = [aws_security_group.sec-group-team4.id]
@@ -25,6 +25,6 @@ module "db" {
   aws_ssm_parameter_name  = "/devops/us-east-1/passwords/team3db-cluster"
   aws_route53_zone        = "devdot.com"
   region                  = var.aws_region
-  db_subnet_group_name    = "team4"
   vpc_security_group_id   = aws_security_group.sec-group-team4.id
+  aws_db_subnet_group     = module.vpc1.private_subnets
 }
